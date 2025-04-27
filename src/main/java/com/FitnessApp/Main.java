69ab15057cb0412c849dccedaf03ddec06946b34
teemubb <166321003+teemubb@users.noreply.github.com>
2025-04-27 04:34:54 +0300

    add db

--- src/main/java/com/FitnessApp/Main.java
+++ src/main/java/com/FitnessApp/Main.java
@@ -1,14 +1,25 @@
 package com.FitnessApp;
+import com.Database.Postgre;
+import java.sql.SQLException;
+
 
 
 public class Main{
     public static void main(String[] args) {
+        Postgre db = new Postgre();
         try {
-            UserInterface ui = new UserInterface(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+            db.open("fitnessapp"); // Open the connection and create tables
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.exit(1);  // Handle connection failure gracefully
+        }
+
+        try {
+            UserInterface ui = new UserInterface(Integer.parseInt(args[0]), Integer.parseInt(args[1]), db);
         } catch (NumberFormatException nbe) {
-            UserInterface ui = new UserInterface(480, 640);
+            UserInterface ui = new UserInterface(480, 640, db);
         } catch (Exception e) {
-            UserInterface ui = new UserInterface(480, 640);
+            UserInterface ui = new UserInterface(480, 640, db);
         }
     }
 }
